1d27f04a4f57383d791665675afd0eeb4cccd792
Nur Alfi Ekowati <nuralfiekowati@gmail.com>
2018-06-23 09:45:44 +0700

    -Update file
    Updating drastic measure.

--- src/owl2inconsistencymeasures/Drastic_jfact.java
+++ src/owl2inconsistencymeasures/Drastic_jfact.java
@@ -4,12 +4,12 @@ import org.semanticweb.owlapi.reasoner.OWLReasoner;
 
 class Drastic_jfact extends inconsistency_measure_jfact {
 
-	public static String Drastic_measure(OWLReasoner reasoner) {
+	public static void Drastic_measure(OWLReasoner reasoner) {
 		
 		if (reasoner.isConsistent()) {
-			return "1. DRASTIC INCONSISTENCY MEASURE I_d: " + 0;
+			System.out.println("1. DRASTIC INCONSISTENCY MEASURE I_d: " + 0);
 		} else {
-			return "1. DRASTIC INCONSISTENCY MEASURE I_d: " + 1;
+			System.out.println("1. DRASTIC INCONSISTENCY MEASURE I_d: " + 1);
 		}
 		
 	}
