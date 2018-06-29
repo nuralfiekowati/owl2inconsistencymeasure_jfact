0117480df370e7ad30c7960a3244936107505d19
Nur Alfi Ekowati <nuralfiekowati@gmail.com>
2018-06-29 11:56:02 +0700

    -Update file
    Updating MC measure.

--- src/owl2inconsistencymeasures/Mc_jfact.java
+++ src/owl2inconsistencymeasures/Mc_jfact.java
@@ -1,66 +1,112 @@
 package owl2inconsistencymeasures;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.NoSuchElementException;
 import java.util.Set;
 
-import org.semanticweb.owl.explanation.api.ExplanationException;
-import org.semanticweb.owl.explanation.api.ExplanationGeneratorInterruptedException;
+import org.semanticweb.owl.explanation.api.Explanation;
 import org.semanticweb.owlapi.apibinding.OWLManager;
-import org.semanticweb.owlapi.model.AxiomType;
-import org.semanticweb.owlapi.model.OWLAsymmetricObjectPropertyAxiom;
+import org.semanticweb.owlapi.model.AddAxiom;
 import org.semanticweb.owlapi.model.OWLAxiom;
-import org.semanticweb.owlapi.model.OWLClass;
-import org.semanticweb.owlapi.model.OWLClassAssertionAxiom;
-import org.semanticweb.owlapi.model.OWLDataFactory;
-import org.semanticweb.owlapi.model.OWLDifferentIndividualsAxiom;
-import org.semanticweb.owlapi.model.OWLDisjointClassesAxiom;
-import org.semanticweb.owlapi.model.OWLDisjointObjectPropertiesAxiom;
-import org.semanticweb.owlapi.model.OWLDisjointUnionAxiom;
-import org.semanticweb.owlapi.model.OWLEquivalentClassesAxiom;
-import org.semanticweb.owlapi.model.OWLEquivalentObjectPropertiesAxiom;
-import org.semanticweb.owlapi.model.OWLFunctionalObjectPropertyAxiom;
-import org.semanticweb.owlapi.model.OWLInverseFunctionalObjectPropertyAxiom;
-import org.semanticweb.owlapi.model.OWLInverseObjectPropertiesAxiom;
-import org.semanticweb.owlapi.model.OWLIrreflexiveObjectPropertyAxiom;
-import org.semanticweb.owlapi.model.OWLNamedIndividual;
-import org.semanticweb.owlapi.model.OWLNegativeObjectPropertyAssertionAxiom;
-import org.semanticweb.owlapi.model.OWLObjectProperty;
-import org.semanticweb.owlapi.model.OWLObjectPropertyAssertionAxiom;
-import org.semanticweb.owlapi.model.OWLObjectPropertyDomainAxiom;
-import org.semanticweb.owlapi.model.OWLObjectPropertyRangeAxiom;
 import org.semanticweb.owlapi.model.OWLOntology;
 import org.semanticweb.owlapi.model.OWLOntologyCreationException;
 import org.semanticweb.owlapi.model.OWLOntologyManager;
-import org.semanticweb.owlapi.model.OWLReflexiveObjectPropertyAxiom;
-import org.semanticweb.owlapi.model.OWLSameIndividualAxiom;
-import org.semanticweb.owlapi.model.OWLSubClassOfAxiom;
-import org.semanticweb.owlapi.model.OWLSubObjectPropertyOfAxiom;
-import org.semanticweb.owlapi.model.OWLSymmetricObjectPropertyAxiom;
-import org.semanticweb.owlapi.model.OWLTransitiveObjectPropertyAxiom;
-import org.semanticweb.owlapi.reasoner.InconsistentOntologyException;
+import org.semanticweb.owlapi.model.OWLOntologyRenameException;
 import org.semanticweb.owlapi.reasoner.OWLReasoner;
-import org.semanticweb.owlapi.reasoner.OWLReasonerConfiguration;
 import org.semanticweb.owlapi.reasoner.OWLReasonerFactory;
 import org.semanticweb.owlapi.reasoner.ReasonerInterruptedException;
-import org.semanticweb.owlapi.reasoner.SimpleConfiguration;
 import org.semanticweb.owlapi.reasoner.TimeOutException;
 
 import uk.ac.manchester.cs.jfact.JFactFactory;
 
-public class Mc_jfact {
+class Mc_jfact {
 
-	float MCsize, SCsize;
-	float imc = (float) MCsize + SCsize - 1;
-	
-	public String Imc_measure() {
-		
-		return "7. MC INCONSISTENCY MEASURE I_mc: " + imc;
-		
+	static Set<OWLAxiom> ontologyAxiomsCausingUnsatisfiable = new HashSet<OWLAxiom>();
+	static HashSet<Set<OWLAxiom>> inconsistentSubset = new HashSet<Set<OWLAxiom>>();
+	static HashSet<Set<OWLAxiom>> consistentSubset = new HashSet<Set<OWLAxiom>>();
+	static ArrayList<Set<OWLAxiom>> MCKcandidate = new ArrayList<Set<OWLAxiom>>();
+
+	public static void Imc_measure(Set<Explanation<OWLAxiom>> explanations, HashSet<OWLAxiom> ontologyAxiomSet) {
+
+		if (explanations.size() > 0) {
+
+			for (Set<OWLAxiom> inconsistent : inconsistentSubset) {
+				for (Set<OWLAxiom> consistent : consistentSubset) {
+					if ((inconsistent.containsAll(consistent) == true) && (inconsistent.equals(consistent) == false)) {
+						// if (inconsistent.size() != consistent.size() &&
+						// consistent.isEmpty() == false) {
+						MCKcandidate.add(consistent);
+						// }
+					}
+				}
+			}
+
+		} else {
+			MCKcandidate.add(ontologyAxiomSet);
+		}
+
+		for (Set<OWLAxiom> mck : NotMCK.eliminate_notMCK(MCKcandidate)) {
+			System.out.println("MCK: " + mck);
+		}
+
+		int MCsize = NotMCK.eliminate_notMCK(MCKcandidate).size();
+
+		// To compute SC(K) in MC Inconsistency Measure
+		try {
+			OWLReasonerFactory rf8 = new JFactFactory(); // for jfact
+			OWLOntologyManager manager8 = OWLManager.createOWLOntologyManager();
+			OWLOntology AxiomOntology8 = manager8.createOntology();
+			Set<OWLAxiom> axiomsToRemove8;
+			AddAxiom addAxiom8;
+
+			for (OWLAxiom theAxiom : ontologyAxiomSet) {
+
+				manager8 = OWLManager.createOWLOntologyManager();
+				AxiomOntology8 = manager8.createOntology();
+
+				axiomsToRemove8 = AxiomOntology8.getAxioms();
+
+				if (axiomsToRemove8 != null) {
+					manager8.removeAxioms(AxiomOntology8, axiomsToRemove8);
+				}
+
+				addAxiom8 = new AddAxiom(AxiomOntology8, theAxiom);
+				manager8.applyChange(addAxiom8);
+
+				OWLReasoner reasoner8 = rf8.createReasoner(AxiomOntology8); // for
+																			// hermit
+																			// and
+																			// JFact
+
+				System.out.println("The axiom: " + theAxiom);
+				System.out
+						.println("Is the axiom (" + theAxiom + ") consistent/satisfiable? " + reasoner8.isConsistent());
+
+				if (reasoner8.isConsistent() == false) {
+					ontologyAxiomsCausingUnsatisfiable.add(theAxiom);
+				}
+
+			}
+		} catch (OWLOntologyRenameException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (TimeOutException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ReasonerInterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (OWLOntologyCreationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		float SCsize = ontologyAxiomsCausingUnsatisfiable.size();
+		System.out.println("MCsize: " + MCsize);
+		System.out.println("SCsize: " + SCsize);
+		float imc = (float) MCsize + SCsize - 1;
+
+		System.out.println("7. MC INCONSISTENCY MEASURE I_mc: " + imc);
+
 	}
 }
