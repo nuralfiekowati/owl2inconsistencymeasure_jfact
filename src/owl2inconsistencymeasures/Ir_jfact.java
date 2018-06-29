eb2e49a9763089cf570cc0b21da0e3b6881b3425
Nur Alfi Ekowati <nuralfiekowati@gmail.com>
2018-06-29 11:33:40 +0700

    -Update file
    Updating file of Iir measure.

--- src/owl2inconsistencymeasures/Ir_jfact.java
+++ src/owl2inconsistencymeasures/Ir_jfact.java
@@ -1,70 +1,28 @@
 package owl2inconsistencymeasures;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.PrintStream;
-import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.NoSuchElementException;
 import java.util.Set;
 
-import org.semanticweb.owl.explanation.api.ExplanationException;
-import org.semanticweb.owl.explanation.api.ExplanationGeneratorInterruptedException;
-import org.semanticweb.owlapi.apibinding.OWLManager;
-import org.semanticweb.owlapi.model.AxiomType;
-import org.semanticweb.owlapi.model.OWLAsymmetricObjectPropertyAxiom;
+import org.semanticweb.owl.explanation.api.Explanation;
 import org.semanticweb.owlapi.model.OWLAxiom;
-import org.semanticweb.owlapi.model.OWLClass;
-import org.semanticweb.owlapi.model.OWLClassAssertionAxiom;
-import org.semanticweb.owlapi.model.OWLDataFactory;
-import org.semanticweb.owlapi.model.OWLDifferentIndividualsAxiom;
-import org.semanticweb.owlapi.model.OWLDisjointClassesAxiom;
-import org.semanticweb.owlapi.model.OWLDisjointObjectPropertiesAxiom;
-import org.semanticweb.owlapi.model.OWLDisjointUnionAxiom;
-import org.semanticweb.owlapi.model.OWLEquivalentClassesAxiom;
-import org.semanticweb.owlapi.model.OWLEquivalentObjectPropertiesAxiom;
-import org.semanticweb.owlapi.model.OWLFunctionalObjectPropertyAxiom;
-import org.semanticweb.owlapi.model.OWLInverseFunctionalObjectPropertyAxiom;
-import org.semanticweb.owlapi.model.OWLInverseObjectPropertiesAxiom;
-import org.semanticweb.owlapi.model.OWLIrreflexiveObjectPropertyAxiom;
-import org.semanticweb.owlapi.model.OWLNamedIndividual;
-import org.semanticweb.owlapi.model.OWLNegativeObjectPropertyAssertionAxiom;
-import org.semanticweb.owlapi.model.OWLObjectProperty;
-import org.semanticweb.owlapi.model.OWLObjectPropertyAssertionAxiom;
-import org.semanticweb.owlapi.model.OWLObjectPropertyDomainAxiom;
-import org.semanticweb.owlapi.model.OWLObjectPropertyRangeAxiom;
-import org.semanticweb.owlapi.model.OWLOntology;
-import org.semanticweb.owlapi.model.OWLOntologyCreationException;
-import org.semanticweb.owlapi.model.OWLOntologyManager;
-import org.semanticweb.owlapi.model.OWLReflexiveObjectPropertyAxiom;
-import org.semanticweb.owlapi.model.OWLSameIndividualAxiom;
-import org.semanticweb.owlapi.model.OWLSubClassOfAxiom;
-import org.semanticweb.owlapi.model.OWLSubObjectPropertyOfAxiom;
-import org.semanticweb.owlapi.model.OWLSymmetricObjectPropertyAxiom;
-import org.semanticweb.owlapi.model.OWLTransitiveObjectPropertyAxiom;
-import org.semanticweb.owlapi.reasoner.InconsistentOntologyException;
-import org.semanticweb.owlapi.reasoner.OWLReasoner;
-import org.semanticweb.owlapi.reasoner.OWLReasonerConfiguration;
-import org.semanticweb.owlapi.reasoner.OWLReasonerFactory;
-import org.semanticweb.owlapi.reasoner.ReasonerInterruptedException;
-import org.semanticweb.owlapi.reasoner.SimpleConfiguration;
-import org.semanticweb.owlapi.reasoner.TimeOutException;
 
-import uk.ac.manchester.cs.jfact.JFactFactory;
+class Ir_jfact {
 
-public class Ir_jfact extends inconsistency_measure_jfact {
-	
-	float I_ir, sizeOfMI, sizeOfK;
-	
-	public String Iir_measure() {
-	
-		if ((sizeOfMI == 0) && (sizeOfK == 0)) {
-			return "6. INCOMPATIBILITY RATIO INCONSISTENCY MEASURE I_ir: 0";
+	public static void Iir_measure(Set<Explanation<OWLAxiom>> explanations, HashSet<OWLAxiom> ontologyAxiomSet) {
+
+		int Ksize = SizeOfK.sizeK(ontologyAxiomSet);
+		float sizeOfMI = explanations.size();
+
+		float I_ir = sizeOfMI / Ksize;
+		System.out.println("Size of MI(K): " + sizeOfMI);
+		System.out.println("Size of K: " + Ksize);
+		if ((sizeOfMI == 0) && (Ksize == 0)) {
+			System.out.println("6. INCOMPATIBILITY RATIO INCONSISTENCY MEASURE I_ir: 0");
 		} else {
-			return "6. INCOMPATIBILITY RATIO INCONSISTENCY MEASURE I_ir: " + I_ir;
+			System.out.println("6. INCOMPATIBILITY RATIO INCONSISTENCY MEASURE I_ir: " + I_ir);
 		}
-		
+		System.out.println("-----------------------------------------------------------------------------");
+
 	}
 
 }
