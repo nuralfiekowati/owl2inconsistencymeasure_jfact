2375a7bbfe4b4c96bcee557151a2ff48f7b9b342
Nur Alfi Ekowati <nuralfiekowati@gmail.com>
2018-09-25 16:30:25 +0700

    -Update drastic
    Modify drastic measure using jfact

--- src/owl2inconsistencymeasures/Drastic_jfact.java
+++ src/owl2inconsistencymeasures/Drastic_jfact.java
@@ -1,10 +1,22 @@
 package owl2inconsistencymeasures;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
+
 import org.semanticweb.owlapi.reasoner.OWLReasoner;
 
 class Drastic_jfact {
 
-	public static void Drastic_measure(OWLReasoner reasoner) {
+	public static void Drastic_measure(OWLReasoner reasoner) throws FileNotFoundException {
+
+		long startTime = System.currentTimeMillis();
+
+		File file = new File("outputs/output_jfact_drastic_New-Feature-BottomObjectProperty-001.txt");
+		FileOutputStream fos = new FileOutputStream(file);
+		PrintStream ps = new PrintStream(fos);
+		System.setOut(ps);
 
 		if (reasoner.isConsistent()) {
 			System.out.println("1. DRASTIC INCONSISTENCY MEASURE I_d: " + 0);
@@ -13,6 +25,8 @@ class Drastic_jfact {
 			System.out.println("1. DRASTIC INCONSISTENCY MEASURE I_d: " + 1);
 			System.out.println("-----------------------------------------------------------------------------");
 		}
+
+		TotalTimeExecution.totalTime(startTime);
 
 	}
 }
